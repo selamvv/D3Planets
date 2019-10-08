7acfb8fca19d51c5f64562ae73e5fff51a35d667
Eric A. Bayer <eric.bayer@centre.edu>
2019-10-08 15:42:19 -0400

    Start.Proj

--- planets.js
+++ planets.js
@@ -1,3 +1,30 @@
+var Names = ["Mercury", "Venus", "Jupiter"]
+d3.select("#A1")
+  .selectAll("div")
+  .data(Names)
+  .enter()
+  .append("div")
+  .text(Names)
+d3.select("#A2")
+  .selectAll("div")
+  .data(Names)
+  .enter()
+  .append("div")
+  .text(function(d) {return d})
+var planPromise = d3.json("planets.json")
+    planPromise.then(function(planets)
+        {
+            console.log(planets)
+        },
+    function(err)
+        {
+            return err
+        })
 
 
- 
+d3.select("#B3")
+  .selectAll("img")
+  .data(planPromise)
+  .enter()
+  .append("img")
+  .attr("src", "bob")
